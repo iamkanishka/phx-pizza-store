3c9332cea894de1a9630fcbf4083185895a0d552
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-15 19:23:34 +0530

    feat: Added accounts domain app

---
+++ apps/pizza_app_accounts/lib/pizza_app_accounts.ex
@@ -0,0 +1,18 @@
+defmodule PizzaAppAccounts do
+  @moduledoc """
+  Documentation for `PizzaAppAccounts`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PizzaAppAccounts.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/pizza_app_accounts/test/pizza_app_accounts_test.exs
@@ -0,0 +1,8 @@
+defmodule PizzaAppAccountsTest do
+  use ExUnit.Case
+  doctest PizzaAppAccounts
+
+  test "greets the world" do
+    assert PizzaAppAccounts.hello() == :world
+  end
+end
