e00d760d591809b6d7ab91d29e9cf0ed4a575a47
Kanishka Naik <39029787+iamkanishka@users.noreply.github.com>
2025-05-15 19:21:16 +0530

    feat: Added payments domain app

---
+++ apps/pizza_app_payments/lib/pizza_app_payments.ex
@@ -0,0 +1,18 @@
+defmodule PizzaAppPayments do
+  @moduledoc """
+  Documentation for `PizzaAppPayments`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PizzaAppPayments.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ apps/pizza_app_payments/test/pizza_app_payments_test.exs
@@ -0,0 +1,8 @@
+defmodule PizzaAppPaymentsTest do
+  use ExUnit.Case
+  doctest PizzaAppPayments
+
+  test "greets the world" do
+    assert PizzaAppPayments.hello() == :world
+  end
+end
